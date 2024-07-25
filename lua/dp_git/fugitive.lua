cd86aaae21c69a78fc529ca432bf7ee5f543cc85
peter-lyr <llydrp.ldp@gmail.com>
2024-07-25 22:22:34 +0800

    M.fugitive_toggle()

--- lua/dp_git/fugitive.lua
+++ lua/dp_git/fugitive.lua
@@ -1,10 +1,16 @@
 local M = {}
 
+local B = require 'dp_base'
+
 function M.fugitive_toggle()
   if vim.o.ft == 'fugitive' then
     vim.cmd 'close'
   else
     vim.cmd 'G'
+    B.set_timeout(10, function()
+      vim.cmd 'set winfixheight'
+      vim.api.nvim_win_set_height(0, 13)
+    end)
   end
 end
 
